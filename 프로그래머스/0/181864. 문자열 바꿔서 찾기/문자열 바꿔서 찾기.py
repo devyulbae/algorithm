45d772780020203557e7290b8bf3771ca26d5069
Seongyul Bae <devyulbae@gmail.com>
2025-05-05 10:28:12 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,2 +1,2 @@
 def solution(myString, pat):
-    return int("".join('B' if pat[x]=='A' else 'A' for x in range(len(pat))) in myString)
+    return int("".join('B' if x=='A' else 'A' for x in pat) in myString)
