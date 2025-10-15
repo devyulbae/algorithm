2713388aed05d420a06fcd759ec7c010194df180
Seongyul Bae <devyulbae@gmail.com>
2025-10-15 19:55:22 +0900

    [Silver V] Title: 나이순 정렬, Time: 200 ms, Memory: 128228 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,14 +1,6 @@
 import sys
-def solution(member):
-    member.sort(key=lambda x: x[0])
-    for age, name in member:
-        print(age, name)
 
-n = int(sys.stdin.readline())
-arr = []
-for i in range(n):
-    a, b = input().split()
-    a = int(a)
-    arr.append([a, b])
-    
-solution(arr)
+data = sys.stdin.readlines()[1:]
+data.sort(key=lambda x:int(x.split()[0]))
+
+print("".join(data))
